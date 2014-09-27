facf017d76abfd40c817d8c5e99d8406957faa1e
Gabriel Wicke <wicke@wikidev.net>
2014-09-27 13:45:06 -0700

    Test & improve 404 handling
    - set proper application/problem+json content-type following
      https://tools.ietf.org/html/draft-nottingham-http-problem
    - add tests for 404 handling at various levels
    - fix a crasher when trying to list buckets in a non-existing domain

--- test/simple.js
+++ test/simple.js
@@ -11,7 +11,8 @@
 var restbase = require('../lib/server.js');
 var preq = require('preq');
 var assert = require('assert');
-var baseURL = 'http://localhost:8888/v1/en.wikipedia.org/test101';
+var baseURL = 'http://localhost:8888/v1/en.wikipedia.org';
+var bucketURL = baseURL + '/test101';
 
 function deepEqual (result, expected) {
     try {
@@ -44,7 +45,7 @@ describe('Simple API tests', function () {
         it('should create a page bucket', function() {
             this.timeout(20000);
             return preq.put({
-                uri: baseURL,
+                uri: bucketURL,
                 headers: { 'content-type': 'application/json' },
                 body: { type: 'pagecontent' }
             })
@@ -52,9 +53,11 @@ describe('Simple API tests', function () {
                 deepEqual(res.status, 201);
             });
         });
+    });
+    describe('Item requests', function() {
         //it('should not accept a new html save without a revision', function() {
         //    return preq.put({
-        //        uri: baseURL + '/Foo/html',
+        //        uri: bucketURL + '/Foo/html',
         //        headers: { 'content-type': 'text/html' },
         //        body: 'Hello there'
         //    })
@@ -65,7 +68,7 @@ describe('Simple API tests', function () {
         it('should transparently create a new HTML revision', function() {
             this.timeout(20000);
             return preq.get({
-                uri: baseURL + '/Foobar/html/624484477',
+                uri: bucketURL + '/Foobar/html/624484477',
                 headers: { 'content-type': 'text/html' },
                 body: 'Hello there'
             })
@@ -75,7 +78,7 @@ describe('Simple API tests', function () {
         });
         it('should return HTML just created', function() {
             return preq.get({
-                uri: baseURL + '/Foobar/html/624484477'
+                uri: bucketURL + '/Foobar/html/624484477'
             })
             .then(function(res) {
                 deepEqual(res.status, 200);
@@ -84,7 +87,7 @@ describe('Simple API tests', function () {
         it('should transparently create data-parsoid, rev 2', function() {
             this.timeout(20000);
             return preq.get({
-                uri: baseURL + '/Foobar/html/624165266'
+                uri: bucketURL + '/Foobar/html/624165266'
             })
             .then(function(res) {
                 deepEqual(res.status, 200);
@@ -92,7 +95,7 @@ describe('Simple API tests', function () {
         });
         it('should return HTML just created, rev 2', function() {
             return preq.get({
-                uri: baseURL + '/Foobar/html/624165266'
+                uri: bucketURL + '/Foobar/html/624165266'
             })
             .then(function(res) {
                 deepEqual(res.status, 200);
@@ -101,7 +104,7 @@ describe('Simple API tests', function () {
         });
         it('should return data-parsoid just created, rev 2', function() {
             return preq.get({
-                uri: baseURL + '/Foobar/data-parsoid/624165266'
+                uri: bucketURL + '/Foobar/data-parsoid/624165266'
             })
             .then(function(res) {
                 deepEqual(res.status, 200);
@@ -110,7 +113,7 @@ describe('Simple API tests', function () {
         });
         it('should accept a new html save with a revision', function() {
             return preq.put({
-                uri: baseURL + '/Foobar/html/76f22880-362c-11e4-9234-0123456789ab',
+                uri: bucketURL + '/Foobar/html/76f22880-362c-11e4-9234-0123456789ab',
                 headers: { 'content-type': 'text/html; charset=UTF-8' },
                 body: 'Hello there'
             })
@@ -124,13 +127,60 @@ describe('Simple API tests', function () {
         });
         it('should return the HTML revision just created', function() {
             return preq.get({
-                uri: baseURL + '/Foobar/html/624484477'
+                uri: bucketURL + '/Foobar/html/624484477'
             })
             .then(function(res) {
                 deepEqual(res.status, 200);
                 deepEqual(res.headers['content-type'], 'text/html; charset=UTF-8');
                 deepEqual(res.headers.etag, '76f22880-362c-11e4-9234-0123456789ab');
                 deepEqual(res.body, 'Hello there');
+            });
+        });
+    });
+    describe('404 handling', function() {
+        it('should return a proper 404 when trying to retrieve a non-existing domain', function() {
+            return preq.get({
+                uri: 'http://localhost:8888/v1/foobar.com'
+            })
+            .catch(function(e) {
+                deepEqual(e.response.status, 404);
+                deepEqual(e.response.headers['content-type'], 'application/problem+json');
+            });
+        });
+        it('should return a proper 404 when trying to list a non-existing domain', function() {
+            return preq.get({
+                uri: 'http://localhost:8888/v1/foobar.com/'
+            })
+            .catch(function(e) {
+                deepEqual(e.response.status, 404);
+                deepEqual(e.response.headers['content-type'], 'application/problem+json');
+            });
+        });
+        it('should return a proper 404 when accessing an unknown bucket', function() {
+            return preq.get({
+                uri: baseURL + '/some_nonexisting_bucket'
+            })
+            .catch(function(e) {
+                deepEqual(e.response.status, 404);
+                deepEqual(e.response.headers['content-type'], 'application/problem+json');
+            });
+        });
+        it('should return a proper 404 when trying to list an unknown bucket', function() {
+            return preq.get({
+                uri: baseURL + '/some_nonexisting_bucket/'
+            })
+            .catch(function(e) {
+                deepEqual(e.response.status, 404);
+                deepEqual(e.response.headers['content-type'], 'application/problem+json');
+            });
+        });
+        it('should return a proper 404 when accessing an item in an unknown bucket', function() {
+            return preq.get({
+                uri: baseURL + '/some_nonexisting_bucket/item'
+            })
+            .catch(function(e) {
+                deepEqual(e.response.status, 404);
+                deepEqual(e.response.headers['content-type'], 'application/problem+json');
             });
         });
     });
