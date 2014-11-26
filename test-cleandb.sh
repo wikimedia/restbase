838c89bc5f614fdc10a916bd7b873f299c4e91d5
James Earl Douglas <james@earldouglas.com>
2014-11-25 17:03:58 -0800

    Move test db cleanup script into test/ directory

--- test-cleandb.sh
+++
@@ -1,6 +0,0 @@
-echo 'drop keyspace "org_wikipedia_en_T_test101_wikitext";
-drop keyspace "org_wikipedia_en_T_test101_dataJtC1RwvsYg2JPEOmY";
-drop keyspace "local_restbase_T_tables";
-drop keyspace "local_restbase_T_domains";
-drop keyspace "org_wikipedia_en_T_test101_datai9WsnfvT2BYdX0tr0";
-drop keyspace "org_wikipedia_en_T_test101_html";' | cqlsh 
