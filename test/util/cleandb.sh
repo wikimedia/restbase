ea7546f9c1064f4114a63c911604faef515cf766
Gabriel Wicke <gwicke@wikimedia.org>
2014-12-09 19:46:23 -0800

    Fix cleandb.sh script
    Note: Those hash sections include the bucket name, so will change when the
    bucket name changes.

--- test/util/cleandb.sh
+++ test/util/cleandb.sh
@@ -1,7 +1,7 @@
 echo 'drop keyspace if exists "local_restbase_T_tables";
 drop keyspace if exists "local_restbase_T_domains";
-drop keyspace if exists "local_test_wikipedia_en_T_page_dataY_5U7x32eajwQ";
-drop keyspace if exists "local_test_wikipedia_en_T_page_html";
-drop keyspace if exists "local_test_wikipedia_en_T_page_rev";
-drop keyspace if exists "local_test_wikipedia_en_T_page_dataUweNiypIUmVNC";
-drop keyspace if exists "local_test_wikipedia_en_T_page_wikitext";' | cqlsh
+drop keyspace if exists "local_test_wikipedia_en_T_pages_dataY_5U7x32eajwQ";
+drop keyspace if exists "local_test_wikipedia_en_T_pages_html";
+drop keyspace if exists "local_test_wikipedia_en_T_pages_rev";
+drop keyspace if exists "local_test_wikipedia_en_T_pages_dataUweNiypIUmVNC";
+drop keyspace if exists "local_test_wikipedia_en_T_pages_wikitext";' | cqlsh
