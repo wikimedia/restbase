b9db5fa90abb8fc66916febc889f5ba922324b18
James Earl Douglas <james@earldouglas.com>
2015-01-01 15:39:50 -0800

    Test the PHP action API with a bad hostname and a bad query
    This revealed a problem in the forwarding a 'localhost' host header
    out to external Web services, which makes the PHP action API rather
    unhappy.

---
+++ test/features/svc/actions.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// mocha defines to avoid JSHint breakage
+/* global describe, it, before, beforeEach, after, afterEach */
+
+var preq   = require('preq');
+var assert = require('../../utils/assert.js');
+
+module.exports = function (config) {
+
+    describe('non-existing api', function() {
+
+        it('should return a 404 when trying to query a non-existing api', function() {
+            this.timeout(20000);
+            return preq.get({
+                uri: config.hostPort + '/v1/www.wikimedia.org/_svc/action/query'
+            })
+            .catch(function(e) {
+                assert.deepEqual(e.status, 404);
+            });
+        });
+
+        it('should return a 200 even when a query returns no results', function() {
+            this.timeout(20000);
+            return preq.get({
+                uri: config.hostPort + '/v1/en.wikipedia.org/_svc/action/query'
+            })
+            .then(function(res) {
+                assert.deepEqual(res.status, 200);
+                assert.deepEqual(res.body.query, undefined);
+                assert.deepEqual(res.body.pages, undefined);
+            });
+        });
+
+    });
+
+};
