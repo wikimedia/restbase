7414f6e2973784b3e4f8aa5e51ac8b0d808e9abb
James Earl Douglas <james@earldouglas.com>
2015-01-13 15:20:25 -0800

    Overhaul spec testing

--- test/features/specification/swagger.js
+++ test/features/specification/swagger.js
@@ -9,47 +9,80 @@ var specs  = require('../../utils/specs.js');
 
 module.exports = function (config) {
 
-    // Wrap an HTTP request in a continuation
-    function requestK(req) {
-        return function () {
-            return preq[req.method](req);
-        };
-    }
-
-    var specUrl = 'http://wikimedia.github.io/restbase/v1/swagger.yaml';
+    var prereqs = [
+        { // create the domain
+            method: 'put',
+            uri: config.hostPort + '/v1/en.wikipedia.test.local',
+            headers: { 'content-type': 'application/json' },
+            body: {},
+        },
+        { // create the bucket
+            method: 'put',
+            uri: config.bucketURL,
+            headers: { 'content-type': 'application/json' },
+            body: { type: 'pagecontent' },
+        },
+        { // transparently create HTML revision id 624484477
+            method: 'get',
+            uri: config.bucketURL + '/Foobar/html/624484477',
+            body: 'Hello there, this is revision 624484477!'
+        },
+        { // create an html revision of Foobar
+            method: 'put',
+            uri: config.bucketURL + '/Foobar/html/76f22880-362c-11e4-9234-0123456789ab',
+            body: 'Hello there, this is revision 76f22880-362c-11e4-9234-0123456789ab!',
+        },
+        { // create an html revision of Foobar
+            method: 'put',
+            uri: config.bucketURL + '/Foobar/html/9843f080-3443-11e4-9234-0123456789ab',
+            body: 'Hello there, this is revision 9843f080-3443-11e4-9234-0123456789ab!',
+        },
+        { // create an html revision of Foobar
+            method: 'put',
+            uri: config.bucketURL + '/Foobar/html/b9f3f880-8153-11e4-9234-0123456789ab',
+            body: 'Hello there, this is revision b9f3f880-8153-11e4-9234-0123456789ab!',
+        },
+    ];
 
     describe('swagger spec', function () {
-        this.timeout(20000);
-        var xamples = [];
+        var xamples = specs.parseXamples(specs.get(), config.hostPort);
 
-        it('should provide testable x-amples', function (done) {
-            specs.getSpec(specUrl, function (spec) {
-                xamples = specs.parseXamples(spec, config.hostPort);
-                var expected = xamples.length;
-                var actual = 0;
-                xamples.reduce(
-                    function (p, xample) {
-                        return p.then(function () {
-                            // Chain the prerequesites in order
-                            return xample.prereqs.reduce(function (p, prereq) {
-                                return p.then(requestK(prereq));
-                            }, Promise.resolve(true))
-                            // Fire off the main request
-                            .then(requestK(xample.request))
-                            // Validate the response
-                            .then(function (res) {
-                                assert.isSuperset(res, xample.response);
-                                actual = actual + 1;
-                            });
-                        });
-                    },
-                    Promise.resolve(true)
-                ).then(function () {
-                    assert.deepEqual(actual, expected);
-                    done();
+        it('should run ' + prereqs.length + ' idempotent prerequisites', function() {
+            var count = 0;
+            var reqChain = prereqs.map(function (req) {
+                return function () { 
+                    return preq[req.method](req)
+                    .then(function (res) {
+                        count = count + 1;
+                        return res;
+                    });
+                };
+            })
+            .reduce(function (f1, f2) {
+                return function () { return f1().then(f2); };
+            });
+            return reqChain()
+            .then(function () {
+                assert.deepEqual(count, prereqs.length, 'only ran ' + count);
+            });
+        });
+
+        var xamplesRun = 0;
+        xamples.forEach(function (xample) {
+            it(xample.description, function() {
+                return preq[xample.request.method](xample.request)
+                .then(function (res) {
+                    assert.isSuperset(res, xample.response);
+                    xamplesRun = xamplesRun + 1;
+                    return res;
                 });
             });
         });
+
+        it('should have run ' + xamples.length + ' xamples', function() {
+            assert.deepEqual(xamplesRun, xamples.length);
+        });
+
     });
 
 };
