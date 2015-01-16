7ce41e392f150dc98a3bd005a5ee10173fcfc5df
Hardik Juneja <hardikjuneja.hj@gmail.com>
2015-01-17 04:27:30 +0530

    Optemize request templating for service requests

--- test/features/router/misc.js
+++ test/features/router/misc.js
@@ -5,6 +5,7 @@
 
 var assert = require('../../utils/assert.js');
 var preq = require('preq');
+var request_parser = require('../../../lib/proxyHandler.js');
 
 module.exports = function (config) {
     
@@ -19,5 +20,19 @@ module.exports = function (config) {
         });
         
     });
+
+    describe('request templating handler', function() {
+        
+        it('should create code out of template', function() {
+        	var assembler = new request_parser()._assemble;
+        	console.log(assembler);
+            var a = assembler("$request");
+            console.log(a);
+            a = assembler({uri:'/foo/bar'});
+            console.log(a);
+        });
+        
+    });
+
     
 };
