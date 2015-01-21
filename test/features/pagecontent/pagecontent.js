1259166b5302e3e6ba1a58fdea37fff76fa2bbc1
Gabriel Wicke <gwicke@wikimedia.org>
2015-01-20 22:52:00 -0800

    Some more fixes
    - Add title & revision listing support
    - Adjust some more tests
    
    In all there are now four failing tests remaining.

--- test/features/pagecontent/pagecontent.js
+++ test/features/pagecontent/pagecontent.js
@@ -80,40 +80,20 @@ module.exports = function (config) {
     });
 
     describe('pagecontent bucket', function() {
-        it('should provide bucket info', function() {
-            this.timeout(20000);
-            return preq.get({
-                uri: config.bucketURL,
-            })
-            .then(function(res) {
-                assert.deepEqual(res.status, 200);
-            });
-        });
+        // TODO: figure out what we'd like to return for /page
+        //it('should provide bucket info', function() {
+        //    this.timeout(20000);
+        //    return preq.get({
+        //        uri: config.bucketURL,
+        //    })
+        //    .then(function(res) {
+        //        assert.deepEqual(res.status, 200);
+        //    });
+        //});
         it('should list its contents', function() {
             this.timeout(20000);
             return preq.get({
                 uri: config.bucketURL + '/',
-            })
-            .then(function(res) {
-                assert.deepEqual(res.status, 200);
-            });
-        });
-    });
-
-    describe('pagecontent/html bucket', function() {
-        it('should provide bucket info', function() {
-            this.timeout(20000);
-            return preq.get({
-                uri: config.bucketURL + '.html',
-            })
-            .then(function(res) {
-                assert.deepEqual(res.status, 200);
-            });
-        });
-        it('should list its contents', function() {
-            this.timeout(20000);
-            return preq.get({
-                uri: config.bucketURL + '.html/',
             })
             .then(function(res) {
                 assert.deepEqual(res.status, 200);
