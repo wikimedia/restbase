82cb51b2eebd2c20c7661e77a2505a51e2e70c2c
Gabriel Wicke <gwicke@wikimedia.org>
2015-01-22 12:20:33 -0800

    Fix re-render tests
    The new key_rev_value code does not create deterministic uuids any more, so
    adjust the tests to check using dynamic uuids instead. Mocha strongly
    discourages sharing of state between tests & by default resets shared
    variables for each test. This means that we need to combine all the re-render
    tests in a single mocha test in order to share dynamic tids.

---
+++ test/features/pagecontent/rerendering.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// mocha defines to avoid JSHint breakage
+/* global describe, it, before, beforeEach, after, afterEach */
+
+var assert = require('../../utils/assert.js');
+var preq = require('preq');
+
+
+module.exports = function (config) {
+
+    describe('page re-rendering', function () {
+        this.timeout(20000);
+
+        var r1 = '615503804';
+        var r2 = '615503846';
+
+
+        function hasTextContentType(res) {
+            var ctype = res.headers['content-type'];
+            if (!/text\/html/.test(ctype)) {
+                throw new Error('Content-type does not match text/html: ' + ctype);
+            }
+        }
+
+
+        it('should render & re-render independent revisions', function () {
+            var r1tid1;
+            var r1tid2;
+            var r2tid1;
+            return preq.get({
+                uri: config.bucketURL + '/Main_Page/html/' + r1
+            })
+            .then(function (res) {
+                assert.deepEqual(res.status, 200);
+                r1tid1 = res.headers.etag;
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r1,
+                    headers: { 'cache-control': 'no-cache' }
+                });
+            })
+            .then(function (res) {
+                r1tid2 = res.headers.etag;
+                assert.notDeepEqual(r1tid2, r1tid1);
+                assert.notDeepEqual(r1tid2, undefined);
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r1 + '/' + r1tid1
+                });
+            })
+            .then(function (res) {
+                assert.deepEqual(res.headers.etag, r1tid1);
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r1
+                });
+            })
+            .then(function (res) {
+                assert.deepEqual(res.headers.etag, r1tid2);
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r1 + '/' + r1tid2
+                });
+            })
+            .then(function (res) {
+                assert.deepEqual(res.headers.etag, r1tid2);
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r2
+                });
+            })
+            .then(function (res) {
+                r2tid1 = res.headers.etag;
+                assert.deepEqual(res.status, 200);
+                hasTextContentType(res);
+
+                // Delay a bit to give the async save time to complete
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r2 + '/' + r2tid1
+                });
+            })
+            .then(function (res) {
+                assert.deepEqual(res.headers.etag, r2tid1);
+                hasTextContentType(res);
+
+                return preq.get({
+                    uri: config.bucketURL + '/Main_Page/html/' + r1
+                });
+            })
+            .then(function (res) {
+                assert.deepEqual(res.headers.etag, r1tid2);
+                hasTextContentType(res);
+            });
+        });
+
+    });
+};
