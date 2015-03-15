40cbe25e6da39e9261987f3e22fb4b3e00fb3d8c
Marko Obrovac <mobrovac@wikimedia.org>
2015-03-15 20:41:03 +0100

    Add tests for the request ID generation and retrieval

--- test/features/router/misc.js
+++ test/features/router/misc.js
@@ -8,10 +8,10 @@ var preq   = require('preq');
 var server = require('../../utils/server.js');
 
 describe('router - misc', function() {
+
     this.timeout(20000);
 
     before(function () { return server.start(); });
-
 
     it('should deny access to /{domain}/sys', function() {
         return preq.get({
@@ -20,5 +20,45 @@ describe('router - misc', function() {
             assert.deepEqual(err.status, 403);
         });
     });
-    
+
+    it('should set a request ID for each sub-request and return it', function() {
+        var slice = server.config.logStream.slice();
+        return preq.get({
+            uri: server.config.bucketURL + '/html/Foobar',
+            headers: {
+                'Cache-Control': 'no-cache'
+            }
+        }).then(function(res) {
+            slice.halt();
+            var reqId = res.headers['x-request-id'];
+            assert.notDeepEqual(reqId, undefined, 'Request ID not returned');
+            slice.get().forEach(function(line) {
+                var a = JSON.parse(line);
+                if(a.req || a.request_id) {
+                    assert.deepEqual(a.request_id, reqId, 'Request ID mismatch');
+                }
+            });
+        });
+    });
+
+    it('should honour the provided request ID', function() {
+        var reqId = 'b6c17ea83d634b31bb28d60aae1caaac';
+        var slice = server.config.logStream.slice();
+        return preq.get({
+            uri: server.config.bucketURL + '/html/Foobar',
+            headers: {
+                'X-Request-Id': reqId
+            }
+        }).then(function(res) {
+            slice.halt();
+            assert.deepEqual(res.headers['x-request-id'], reqId, 'Returned request ID does not match the sent one');
+            slice.get().forEach(function(line) {
+                var a = JSON.parse(line);
+                if(a.req || a.request_id) {
+                    assert.deepEqual(a.request_id, reqId, 'Request ID mismatch');
+                }
+            });
+        });
+    });
+
 });
