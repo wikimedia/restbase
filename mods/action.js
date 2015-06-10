437528ab77aed67260637b8b576f51f3f00f41bb
Marko Obrovac <mobrovac@wikimedia.org>
2015-06-10 13:19:18 +0200

    Reformulate error code detection

--- mods/action.js
+++ mods/action.js
@@ -6,6 +6,101 @@
 
 var rbUtil = require('../lib/rbUtil');
 
+/**
+ * Error translation
+ */
+var errDefs = {
+    '400': { status: 400, type: 'invalid_request' },
+    '401': { status: 401, type: 'unauthorized' },
+    '403': { status: 403, type: 'access_denied#edit' },
+    '409': { status: 409, type: 'conflict' },
+    '413': { status: 413, type: 'too_large' },
+    '429': { status: 429, type: 'rate_exceeded' },
+    '500': { status: 500, type: 'server_error' },
+    '501': { status: 501, type: 'not_supported' }
+};
+var errCodes = {
+    /* 400 - bad request */
+    'articleexists': errDefs['400'],
+    'badformat': errDefs['400'],
+    'badmd5': errDefs['400'],
+    'badtoken': errDefs['400'],
+    'invalidparammix': errDefs['400'],
+    'invalidsection': errDefs['400'],
+    'invalidtitle': errDefs['400'],
+    'invaliduser': errDefs['400'],
+    'missingparam': errDefs['400'],
+    'missingtitle': errDefs['400'],
+    'nosuchpageid': errDefs['400'],
+    'nosuchrcid': errDefs['400'],
+    'nosuchrevid': errDefs['400'],
+    'nosuchsection': errDefs['400'],
+    'nosuchuser': errDefs['400'],
+    'notext': errDefs['400'],
+    'notitle': errDefs['400'],
+    'pagecannotexist': errDefs['400'],
+    'revwrongpage': errDefs['400'],
+    /* 401 - unauthorised */
+    'cantcreate-anon': errDefs['401'],
+    'confirmemail': errDefs['401'],
+    'noedit-anon': errDefs['401'],
+    'noimageredirect-anon': errDefs['401'],
+    'protectedpage': errDefs['401'],
+    'readapidenied': errDefs['401'],
+    /* 403 - access denied */
+    'autoblocked': errDefs['403'],
+    'blocked': errDefs['403'],
+    'cantcreate': errDefs['403'],
+    'customcssjsprotected': errDefs['403'],
+    'customcssprotected': errDefs['403'],
+    'customjsprotected': errDefs['403'],
+    'emptynewsection': errDefs['403'],
+    'emptypage': errDefs['403'],
+    'filtered': errDefs['403'],
+    'hookaborted': errDefs['403'],
+    'noedit': errDefs['403'],
+    'noimageredirect': errDefs['403'],
+    'permissiondenied': errDefs['403'],
+    'protectednamespace': errDefs['403'],
+    'protectednamespace-interface': errDefs['403'],
+    'protectedtitle': errDefs['403'],
+    'readonly': errDefs['403'],
+    'unsupportednamespace': errDefs['403'],
+    'writeapidenied': errDefs['403'],
+    /* 409 - conflict */
+    'cascadeprotected': errDefs['409'],
+    'editconflict': errDefs['409'],
+    'pagedeleted': errDefs['409'],
+    'spamdetected': errDefs['409'],
+    /* 413 - body too large */
+    'contenttoobig': errDefs['413'],
+    /* 429 - rate limit exceeded */
+    'ratelimited': errDefs['429'],
+    /* 501 - not supported */
+    'editnotsupported': errDefs['501']
+};
+function apiError(apiErr) {
+    var ret;
+    apiErr = apiErr || {};
+    ret = {
+        message: 'MW API call error ' + apiErr.code,
+        status: errDefs['500'].status,
+        body: {
+            type: errDefs['500'].type,
+            title: apiErr.code || 'MW API Error',
+            description: apiErr.info || 'Unknown MW API error'
+        }
+    };
+    if(apiErr.code && errCodes.hasOwnProperty(apiErr.code)) {
+        ret.status = errCodes[apiErr.code].status;
+        ret.body.type = errCodes[apiErr.code].type;
+    }
+    return new rbUtil.HTTPError(ret);
+}
+
+/**
+ * Action module code
+ */
 function ActionService (options) {
     this.apiURITemplate = options.apiURI;
 }
@@ -14,108 +109,6 @@ ActionService.prototype.apiURI = function(domain) {
     // TODO: use proper templating
     return this.apiURITemplate.replace(/\{domain\}/, domain);
 };
-
-function apiError(apiErr) {
-    var ret;
-    apiErr = apiErr || {};
-    ret = {
-        status: 500,
-        body: {
-            type: 'server_error',
-            title: apiErr.code || 'MW API Error',
-            description: apiErr.info || 'Unknown MW API error'
-        }
-    };
-    if(!apiErr.code) {
-        return new rbUtil.HTTPError(ret);
-    }
-    switch(apiErr.code) {
-        /* 400 - bad request */
-        case 'articleexists':
-        case 'badformat':
-        case 'badmd5':
-        case 'badtoken':
-        case 'invalidparammix':
-        case 'invalidsection':
-        case 'invalidtitle':
-        case 'invaliduser':
-        case 'missingparam':
-        case 'missingtitle':
-        case 'nosuchpageid':
-        case 'nosuchrcid':
-        case 'nosuchrevid':
-        case 'nosuchsection':
-        case 'nosuchuser':
-        case 'notext':
-        case 'notitle':
-        case 'pagecannotexist':
-        case 'revwrongpage':
-            ret.status = 400;
-            ret.body.type = 'invalid_request';
-            break;
-        /* 401 - unauthorised */
-        case 'cantcreate-anon':
-        case 'confirmemail':
-        case 'noedit-anon':
-        case 'noimageredirect-anon':
-        case 'protectedpage':
-        case 'readapidenied':
-            ret.status = 401;
-            ret.body.type = 'unauthorized';
-            break;
-        /* 403 - access denied */
-        case 'autoblocked':
-        case 'blocked':
-        case 'cantcreate':
-        case 'customcssjsprotected':
-        case 'customcssprotected':
-        case 'customjsprotected':
-        case 'emptynewsection':
-        case 'emptypage':
-        case 'noedit':
-        case 'noimageredirect':
-        case 'permissiondenied':
-        case 'protectednamespace':
-        case 'protectednamespace-interface':
-        case 'protectedtitle':
-        case 'readonly':
-        case 'writeapidenied':
-            ret.status = 403;
-            ret.body.type = 'access_denied#edit';
-            break;
-        /* 409 - conflict */
-        case 'cascadeprotected':
-        case 'editconflict':
-        case 'pagedeleted':
-        case 'spamdetected':
-            ret.status = 409;
-            ret.body.type = 'conflict';
-            break;
-        /* 412 - precondition failed */
-        case 'filtered':
-        case 'hookaborted':
-        case 'unsupportednamespace':
-            ret.status = 412;
-            ret.body.type = 'precondition_fail';
-            break;
-        /* 413 - body too large */
-        case 'contenttoobig':
-            ret.status = 413;
-            ret.body.type = 'too_large';
-            break;
-        /* 429 - rate limit exceeded */
-        case 'ratelimited':
-            ret.status = 429;
-            ret.body.type = 'rate_exceeded';
-            break;
-        /* 501 - not supported */
-        case 'editnotsupported':
-            ret.status = 501;
-            ret.body.type = 'not_supported';
-            break;
-    }
-    return new rbUtil.HTTPError(ret);
-}
 
 function buildQueryResponse(res) {
     if (res.status !== 200) {
