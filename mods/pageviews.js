3d54e124b1827030dc196cac1b5563f047a07e50
Marcel Ruiz Forns <mforns@wikimedia.org>
2015-07-17 20:22:07 -0500

    Add analytics pageview module, spec, and tests
    Bug: T107053

---
+++ mods/pageviews.js
@@ -0,0 +1,297 @@
+'use strict';
+
+/**
+ * Pageviews API module
+ *
+ * Main tasks:
+ * - TBD
+ */
+
+
+var URI = require('swagger-router').URI;
+
+// TODO: move to module
+var fs = require('fs');
+var yaml = require('js-yaml');
+var path = require('path');
+var spec = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '/pageviews.yaml')));
+
+
+// Pageviews Service
+function PJVS (options) {
+    this.options = options;
+    this.log = options.log || function(){};
+}
+
+
+var tables = {
+        article: 'pageviews.per.article',
+        project: 'pageviews.per.project',
+        tops: 'top.pageviews',
+    },
+    tableURI = function(domain, tableName) {
+        return new URI([domain, 'sys', 'table', tableName, '']);
+    },
+    tableSchemas = {
+        article: {
+            table: tables.article,
+            version: 1,
+            attributes: {
+                project     : 'string',
+                article     : 'string',
+                access      : 'string',
+                agent       : 'string',
+                granularity : 'string',
+                // the hourly timestamp will be stored as YYYYMMDDHH
+                timestamp   : 'string',
+                views       : 'int'
+            },
+            index: [
+                { attribute: 'project', type: 'hash' },
+                { attribute: 'article', type: 'hash' },
+                { attribute: 'access', type: 'hash' },
+                { attribute: 'agent', type: 'hash' },
+                { attribute: 'granularity', type: 'hash' },
+                { attribute: 'timestamp', type: 'range', order: 'asc' },
+            ]
+        },
+        project: {
+            table: tables.project,
+            version: 1,
+            attributes: {
+                project     : 'string',
+                access      : 'string',
+                agent       : 'string',
+                granularity : 'string',
+                // the hourly timestamp will be stored as YYYYMMDDHH
+                timestamp   : 'string',
+                views       : 'int'
+            },
+            index: [
+                { attribute: 'project', type: 'hash' },
+                { attribute: 'access', type: 'hash' },
+                { attribute: 'agent', type: 'hash' },
+                { attribute: 'granularity', type: 'hash' },
+                { attribute: 'timestamp', type: 'range', order: 'asc' },
+            ]
+        },
+        tops: {
+            table: tables.tops,
+            version: 1,
+            attributes: {
+                project  : 'string',
+                access   : 'string',
+                year     : 'string',
+                month    : 'string',
+                day      : 'string',
+                // format for this is a json array: [{rank: 1, article: <<title>>, views: 123}, ...]
+                articles : 'string'
+            },
+            index: [
+                { attribute: 'project', type: 'hash' },
+                { attribute: 'access', type: 'hash' },
+                { attribute: 'year', type: 'hash' },
+                { attribute: 'month', type: 'hash' },
+                { attribute: 'day', type: 'hash' },
+            ]
+        }
+    };
+
+/* general handler functions */
+var queryCatcher = function (e) {
+        if (e.status !== 404) {
+            throw e;
+        }
+    },
+    queryResponser = function (res) {
+        // always return at least an empty array so that queries for non-existing data don't error
+        res = res || {};
+        res.body = res.body || {items:[]};
+        res.headers = res.headers || {};
+        res.status = res.status || 200;
+        return res;
+    };
+
+
+PJVS.prototype.pageviewsForArticle = function (restbase, req) {
+    var rp = req.params,
+        dataRequest;
+
+    dataRequest = restbase.get({
+        uri: tableURI(rp.domain, tables.article),
+        body: {
+            table: tables.article,
+            attributes: {
+                project: rp.project,
+                access: rp.access,
+                agent: rp.agent,
+                article: rp.article,
+                granularity: rp.granularity,
+                timestamp: { between: [rp.start, rp.end] },
+            }
+        }
+
+    }).catch(queryCatcher);
+
+    return dataRequest.then(queryResponser);
+};
+
+PJVS.prototype.pageviewsForProjects = function (restbase, req) {
+    var rp = req.params,
+        dataRequest;
+
+    dataRequest = restbase.get({
+        uri: tableURI(rp.domain, tables.project),
+        body: {
+            table: tables.project,
+            attributes: {
+                project: rp.project,
+                access: rp.access,
+                agent: rp.agent,
+                granularity: rp.granularity,
+                timestamp: { between: [rp.start, rp.end] },
+            }
+        }
+
+    }).catch(queryCatcher);
+
+    return dataRequest.then(queryResponser);
+};
+
+PJVS.prototype.pageviewsForTops = function (restbase, req) {
+    var rp = req.params,
+        dataRequest;
+
+    if (rp.year === 'all-years') {
+        rp.month = 'all-months';
+    }
+    if (rp.month === 'all-months') {
+        rp.day = 'all-days';
+    }
+
+    dataRequest = restbase.get({
+        uri: tableURI(rp.domain, tables.tops),
+        body: {
+            table: tables.tops,
+            attributes: {
+                project: rp.project,
+                access: rp.access,
+                year: rp.year,
+                month: rp.month,
+                day: rp.day
+            }
+        }
+
+    }).catch(queryCatcher);
+
+    return dataRequest.then(queryResponser);
+};
+
+
+// The following three functions are just for testing
+
+PJVS.prototype.insertPageviewsForArticleTestData = function(restbase, req) {
+    var rp = req.params,
+        lastPromise;
+
+    lastPromise = restbase.put({ // Save / update the pageviews entry
+        uri: tableURI(rp.domain, tables.article),
+        body: {
+            table: tables.article,
+            attributes: {
+                project: rp.project,
+                article: rp.article,
+                granularity: rp.granularity,
+                access: rp.access,
+                agent: rp.agent,
+                timestamp: rp.timestamp,
+                views: rp.views,
+            },
+        }
+    });
+
+    return lastPromise;
+};
+
+
+PJVS.prototype.insertPageviewsForProjectTestData = function(restbase, req) {
+    var rp = req.params,
+        lastPromise;
+
+    lastPromise = restbase.put({ // Save / update the pageviews entry
+        uri: tableURI(rp.domain, tables.project),
+        body: {
+            table: tables.project,
+            attributes: {
+                project: rp.project,
+                granularity: rp.granularity,
+                access: rp.access,
+                agent: rp.agent,
+                timestamp: rp.timestamp,
+                views: rp.views,
+            },
+        }
+    });
+
+    return lastPromise;
+};
+
+
+
+PJVS.prototype.insertPageviewsForTopsTestData = function(restbase, req) {
+    var rp = req.params,
+        lastPromise;
+
+    lastPromise = restbase.put({ // Save / update the pageviews entry
+        uri: tableURI(rp.domain, tables.tops),
+        body: {
+            table: tables.tops,
+            attributes: {
+                project: rp.project,
+                access: rp.access,
+                year: rp.year,
+                month: rp.month,
+                day: rp.day,
+                articles: JSON.stringify([
+                    {rank: rp.rank, article: rp.article + rp.timespan, views: rp.views},
+                ]),
+            },
+        }
+    });
+
+    return lastPromise;
+};
+
+
+module.exports = function(options) {
+    var pjvs = new PJVS(options);
+
+    return {
+        spec: spec,
+        operations: {
+            pageviewsForArticle: pjvs.pageviewsForArticle.bind(pjvs),
+            pageviewsForProjects: pjvs.pageviewsForProjects.bind(pjvs),
+            pageviewsForTops: pjvs.pageviewsForTops.bind(pjvs),
+
+            // These operations just insert fake data, uncomment them above to play
+            insertPageviewsForArticleTestData: pjvs.insertPageviewsForArticleTestData.bind(pjvs),
+            insertPageviewsForProjectTestData: pjvs.insertPageviewsForProjectTestData.bind(pjvs),
+            insertPageviewsForTopsTestData: pjvs.insertPageviewsForTopsTestData.bind(pjvs),
+        },
+        resources: [
+            {
+                // pageviews per article table
+                uri: '/{domain}/sys/table/' + tables.article,
+                body: tableSchemas.article,
+            }, {
+                // pageviews per project table
+                uri: '/{domain}/sys/table/' + tables.project,
+                body: tableSchemas.project,
+            }, {
+                // top pageviews table
+                uri: '/{domain}/sys/table/' + tables.tops,
+                body: tableSchemas.tops,
+            }
+        ]
+    };
+};
---
+++ test/features/pageviews/pageviews.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// mocha defines to avoid JSHint breakage
+/* global describe, it, before, beforeEach, afterEach */
+
+var assert = require('../../utils/assert.js');
+var preq   = require('preq');
+var server = require('../../utils/server.js');
+
+
+describe('pageviews endpoints', function () {
+    this.timeout(20000);
+
+    //Start server before running tests
+    before(function () { return server.start(); });
+
+    var articleEndpoint = '/pageviews/per-article/en.wikipedia/desktop/spider/one/daily/2015070100/2015070300';
+    var projectEndpoint = '/pageviews/per-project/en.wikipedia/mobile-app/spider/hourly/2015070100/2015070102';
+    var topsEndpoint = '/pageviews/top/en.wikipedia/mobile-web/2015/all-months/30';
+
+    // Fake data insertion endpoints
+    var insertArticleEndpoint = '/pageviews/insert-per-article/en.wikipedia/desktop/spider/one/daily/2015070200';
+    var insertProjectEndpoint = '/pageviews/insert-per-project/en.wikipedia/mobile-app/spider/hourly/2015070101';
+    var insertTopsEndpoint = '/pageviews/insert-top/en.wikipedia/mobile-web/2015/all-months/all-days/1/one';
+
+    // Test Article Endpoint
+
+    it('should return empty when no per article data is available', function () {
+        return preq.get({
+            uri: server.config.baseURL + articleEndpoint
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 0);
+        });
+    });
+
+    it('should return the expected per article data after insertion', function () {
+        return preq.get({
+            uri: server.config.baseURL + insertArticleEndpoint + '/100'
+        }).then(function (res){
+            return preq.get({
+                uri: server.config.baseURL + articleEndpoint
+            })
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 1);
+            assert.deepEqual(res.body.items[0].views, 100);
+        });
+    });
+
+
+    // Test Project Endpoint
+
+    it('should return empty when no per project data is available', function () {
+        return preq.get({
+            uri: server.config.baseURL + projectEndpoint
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 0);
+        });
+    });
+
+    it('should return the expected per project data after insertion', function () {
+        return preq.get({
+            uri: server.config.baseURL + insertProjectEndpoint + '/1000'
+        }).then(function (res){
+            return preq.get({
+                uri: server.config.baseURL + projectEndpoint
+            })
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 1);
+            assert.deepEqual(res.body.items[0].views, 1000);
+        });
+    });
+
+
+    // Test Top Endpoint
+
+    it('should return empty when no tops data is available', function () {
+        return preq.get({
+            uri: server.config.baseURL + topsEndpoint
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 0);
+        });
+    });
+
+    it('should return the expected tops data after insertion', function () {
+        return preq.get({
+            uri: server.config.baseURL + insertTopsEndpoint + '/2000'
+        }).then(function (res){
+            return preq.get({
+                uri: server.config.baseURL + topsEndpoint
+            })
+        }).then(function (res) {
+            assert.deepEqual(res.body.items.length, 1);
+            var article = JSON.parse(res.body.items[0].articles)[0];
+            assert.deepEqual(article.views, 2000);
+        });
+    });
+});
