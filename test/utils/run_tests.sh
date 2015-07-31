65b36a56019e040948ffca3088ed183aa4388947
Petr Pchelko <ppchelko@wikimedia.org>
2015-07-31 17:42:57 +0300

    More updates to the test runner script: quoting, then syntax

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -5,8 +5,7 @@ mocha="$mod_dir"/mocha/bin/mocha
 istanbul="$mod_dir"/istanbul/lib/cli.js
 
 runTest ( ) {
-    if [ "$1" = "sqlite" ]
-    then
+    if [ "$1" = "sqlite" ]; then
         echo "Running with SQLite backend"
         export RB_TEST_BACKEND=sqlite
         rm -f restbase
@@ -16,35 +15,29 @@ runTest ( ) {
         sh ./test/utils/cleandb.sh
     fi
 
-    if [ "$2" = "test" ]
-    then
-        ${mocha}
-    elif [ "$2" = "coverage" ]
-    then
-        ${istanbul} cover node_modules/.bin/_mocha -- -R spec
+    if [ "$2" = "test" ]; then
+        "${mocha}"
+    elif [ "$2" = "coverage" ]; then
+        "${istanbul}" cover node_modules/.bin/_mocha -- -R spec
     fi
 }
 
-if [ "x$2" == "x" ]
-then
+if [ "x$2" == "x" ]; then
     # no concrete backend is provided, check for cassandra
     `echo exit;` | cqlsh 2> /dev/null
-    if [ "$?" -eq 0 ]
-    then
+    if [ "$?" -eq 0 ]; then
         runTest "cassandra" $1
     else
         echo "Cassandra not available. Using SQLite backed for tests"
         runTest "sqlite" $1
     fi
-elif [ "$2" = "sqlite" ]
-then
+elif [ "$2" = "sqlite" ]; then
     runTest "sqlite" $1
-elif [ "$2" = "cassandra" ]
-then
+elif [ "$2" = "cassandra" ]; then
     runTest "cassandra" $1
-elif [ "$2" = "all" ]
-then
-    runTest "sqlite" $1 && runTest "cassandra" $1
+elif [ "$2" = "all" ]; then
+    runTest "sqlite" $1
+    runTest "cassandra" $1
 else
     echo "Invalid testing mode"
     exit 1
