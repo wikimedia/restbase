32d6cf07722f93d54be0ac5464050cf3cca678c8
Petr Pchelko <ppchelko@wikimedia.org>
2015-08-13 13:54:21 +0300

    Fixed jscs and lint warnings

--- maintenance/update_all_revisions.js
+++ maintenance/update_all_revisions.js
@@ -24,7 +24,8 @@ function updateDomain(domain, next) {
     })
     .then(function(res) {
         if (res && res.body && res.body.items) {
-            console.log('Processing page of ' + domain + '. Page starts from article: "' + res.body.items[0] + '"');
+            console.log('Processing page of ' + domain +
+                '. Page starts from article: "' + res.body.items[0] + '"');
             return P.all(res.body.items.map(function(title) {
                 return preq.get({
                     uri: restBaseBaseUri + domain + '/v1/page/title/' + encodeURIComponent(title),
@@ -42,7 +43,7 @@ function updateDomain(domain, next) {
             }))
             .then(function() {
                 if (res.body._links && res.body._links.next) {
-                    return updateDomain(domain, res.body._links.next.href)
+                    return updateDomain(domain, res.body._links.next.href);
                 }
             });
         }
