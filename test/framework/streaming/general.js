c170d0246e3190f9e3a169ec6cad0b0731d041dd
Gabriel Wicke <gwicke@wikimedia.org>
2016-01-26 14:19:51 -0800

    Add streaming tests

---
+++ test/framework/streaming/general.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var assert = require('../utils/assert.js');
+var Server = require('../utils/server.js');
+var preq = require('preq');
+
+// mocha defines to avoid JSHint breakage
+/* global describe, it, before, beforeEach, after, afterEach */
+
+describe('Handler Template', function() {
+    var server = new Server('test/framework/streaming/test_config.yaml');
+    before(function() { return server.start(); });
+
+    it('Basic streaming', function () {
+        return preq.get({ uri: server.hostPort + '/test/hello' })
+        .then(function (res) {
+            assert.deepEqual(res.status, 200);
+            assert.deepEqual(res.headers['content-type'], 'text/html');
+            assert.deepEqual(res.body, 'hello');
+        });
+    });
+
+    it('Buffer streaming', function () {
+        return preq.get({ uri: server.hostPort + '/test/buffer' })
+        .then(function (res) {
+            assert.deepEqual(res.status, 200);
+            assert.deepEqual(res.headers['content-type'], 'text/html');
+            assert.deepEqual(res.body, 'hello');
+        });
+    });
+
+    it('Multi-chunk streaming', function () {
+        return preq.get({ uri: server.hostPort + '/test/chunks' })
+        .then(function (res) {
+            assert.deepEqual(res.status, 200);
+            assert.deepEqual(res.headers['content-type'], 'text/html');
+            if (!/^0123456.*99$/.test(res.body)) {
+                throw new Error('Expected the body to match /^0123456.*99$/');
+            }
+        });
+    });
+
+    after(function() { return server.stop(); });
+});
---
+++ test/framework/streaming/stream_module.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var stream = require('stream');
+
+function hello(restbase, req) {
+    var body = new stream.PassThrough();
+    body.end('hello');
+    return {
+        status: 200,
+        headers: {
+            'content-type': 'text/html',
+        },
+        body: body
+    };
+}
+
+function buffer(restbase, req) {
+    var body = new stream.PassThrough();
+    body.end(new Buffer('hello'));
+    return {
+        status: 200,
+        headers: {
+            'content-type': 'text/html',
+        },
+        body: body
+    };
+}
+
+function chunks(restbase, req) {
+    var body = new stream.PassThrough();
+    for (var i = 0; i < 100; i++) {
+        body.write(i.toString());
+    }
+    body.end();
+    return {
+        status: 200,
+        headers: {
+            'content-type': 'text/html',
+        },
+        body: body
+    };
+}
+
+module.exports = function(options) {
+    return {
+        spec: {
+            paths: {
+                '/hello': {
+                    get: {
+                        operationId: 'hello'
+                    }
+                },
+                '/buffer': {
+                    get: {
+                        operationId: 'buffer'
+                    }
+                },
+                '/chunks': {
+                    get: {
+                        operationId: 'chunks'
+                    }
+                }
+            }
+        },
+        operations: {
+            hello: hello,
+            buffer: buffer,
+            chunks: chunks,
+        }
+    };
+};
