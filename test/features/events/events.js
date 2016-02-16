c23fe404f836bf43f19a4c92b33071c5d09d1e48
Petr Pchelko <ppchelko@wikimedia.org>
2016-02-16 11:28:17 -0800

    Improved coverage of the events module

--- test/features/events/events.js
+++ test/features/events/events.js
@@ -25,9 +25,11 @@ describe('Change event emitting', function() {
                     [ '//en.wikipedia.org/api/rest_v1/page/mobile-sections-lead/Test',
                         '//en.wikipedia.org/api/rest_v1/page/mobile-sections-remaining/Test',
                         '//en.wikipedia.org/api/rest_v1/page/mobile-sections/Test' ]);
+                    udpServer.close();
                     done();
                 }
             } catch (e) {
+                udpServer.close();
                 done(e);
             }
         });
@@ -38,6 +40,54 @@ describe('Change event emitting', function() {
             headers: {
                 'cache-control': 'no-cache'
             }
+        })
+        .delay(100)
+        .finally(function() {
+            udpServer.close();
+        });
+    });
+
+    it('should not explode if events config is not provided', function() {
+        return preq.post({
+            uri: server.config.baseURL + '/events_no_config/',
+            body: [
+                { uri: '//en.wikipedia.org' }
+            ]
+        });
+    });
+
+    it('should not explode on incorrect body', function() {
+        return preq.post({
+            uri: server.config.baseURL + '/events/',
+            body: { uri: '//en.wikipedia.org' }
+        });
+    });
+
+    it('should send valid events and drop invalid', function(done) {
+        var udpServer = dgram.createSocket('udp4');
+
+        udpServer.on("message", function(msg) {
+            try {
+                var uri = msg.slice(22, 22 + msg.readInt16BE(20)).toString();
+                assert.deepEqual(uri, '//en.wikipedia.org');
+                udpServer.close();
+                done();
+            } catch (e) {
+                udpServer.close();
+                done(e);
+            }
+        });
+        udpServer.bind(4321);
+
+        return preq.post({
+            uri: server.config.baseURL + '/events/',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: [
+                { uri: '//en.wikipedia.org' },
+                { should_not_be: 'here' }
+            ]
         })
         .delay(100)
         .finally(function() {
