6f0d448b437cd22d44d0424762dffe11fd502f93
Petr Pchelko <ppchelko@wikimedia.org>
2016-02-17 18:16:27 -0800

    Removed toString() calls

--- sys/events.js
+++ sys/events.js
@@ -27,13 +27,13 @@ EventService.prototype.emitEvent = function(hyper, req) {
             }
 
             return self.purger.purge(req.body.map(function(event) {
-                if (!event.meta || !event.meta.uri || !/^\/\//.test(event.meta.uri.toString())) {
+                if (!event.meta || !event.meta.uri || !/^\/\//.test(event.meta.uri)) {
                     hyper.log('error/events/purge', {
                         message: 'Invalid event URI',
                         event: event
                     });
                 } else {
-                    return 'http:' + event.meta.uri.toString();
+                    return 'http:' + event.meta.uri;
                 }
             })
             .filter(function(event) { return !!event; }));
