4d6cdc35a529f40158d5efff00ceefc912171fdc
Petr Pchelko <ppchelko@wikimedia.org>
2016-07-21 10:50:57 -0700

    Refetch siteInfo in case it failed for some reason

--- test/features/errors/invalid_request.js
+++ test/features/errors/invalid_request.js
@@ -3,14 +3,79 @@
 // mocha defines to avoid JSHint breakage
 /* global describe, it, before, beforeEach, after, afterEach */
 
-var assert = require('../../utils/assert.js');
-var preq   = require('preq');
-var server = require('../../utils/server.js');
+var assert  = require('../../utils/assert.js');
+var preq    = require('preq');
+var server  = require('../../utils/server.js');
+var nock    = require('nock');
+var mwUtils =
 
 describe('400 handling', function() {
     this.timeout(20000);
 
-    before(function () { return server.start(); });
+    var siteInfo;
+    var revisionInfo;
+    before(function () {
+        return server.start()
+        .then(function() {
+            // Fetch real siteInfo to return from a mock
+            return preq.post({
+                uri: server.config.apiURL,
+                body: {
+                    action: 'query',
+                    meta: 'siteinfo|filerepoinfo',
+                    siprop: 'general|namespaces|namespacealiases',
+                    format: 'json',
+                    formatversion: 2
+                }
+            });
+        })
+        .then(function(res) {
+            siteInfo = res.body;
+            // Fetch real revision info for Main_Page
+            return preq.post({
+                uri: server.config.apiURL,
+                body: {
+                    action: 'query',
+                    prop: 'info|revisions',
+                    continue: '',
+                    rvprop: 'ids|timestamp|user|userid|size|sha1|contentmodel|comment|tags',
+                    format: 'json',
+                    formatversion: 2,
+                    titles: 'Main_Page'
+                }
+            });
+        })
+        .then(function(res) {
+            revisionInfo = res.body;
+        })
+    });
+
+    it('should refetch siteInfo on error', function() {
+        // Set up nock:
+        // 1. Throw an error on siteInfo fetch
+        // 2. Return correct siteInfo
+        // 3. Return revision data
+        var mwApi = nock(server.config.apiURL, {allowUnmocked: true})
+        .post('').reply(400)
+        .post('').reply(200, siteInfo)
+        .post('').reply(200, revisionInfo);
+
+        return preq.get({
+            uri: server.config.bucketURL + '/title/Main_Page'
+        })
+        .catch(function (e) {
+            assert.deepEqual(e.status, 400);
+            return preq.get({
+                uri: server.config.bucketURL + '/title/Main_Page'
+            });
+        })
+        .then(function(res) {
+            assert.deepEqual(res.status, 200);
+            assert.deepEqual(res.body.items[0].title, 'Main_Page');
+            mwApi.done();
+        })
+        .finally(function () { nock.cleanAll(); })
+    });
 
     it('should return a proper 400 for an empty POST', function() {
         return preq.post({
