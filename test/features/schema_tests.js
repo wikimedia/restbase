f43210996f6aad786a109df2cb866f1d3ef514f3
Petr Pchelko <ppchelko@wikimedia.org>
2016-09-08 15:41:48 -0700

    Removed ES6 features

--- test/features/schema_tests.js
+++ test/features/schema_tests.js
@@ -9,27 +9,20 @@ var preq   = require('preq');
 var Ajv = require('ajv');
 
 describe('Responces should conform to the provided JSON schema of the responce', function() {
-
-    var spec;
-    var ajv;
+    var ajv = new Ajv({});
 
     before(function() {
         return server.start()
-        .then(() => {
-            return preq.get({ uri: server.config.baseURL + '/?spec' });
-        })
-        .then((res) => {
-            ajv = new Ajv();
+        .then(function() { return preq.get({ uri: server.config.baseURL + '/?spec' }); })
+        .then(function(res) {
             Object.keys(res.body.definitions).forEach(function(defName) {
                 ajv.addSchema(res.body.definitions[defName], '#/definitions/' + defName);
             });
         });
     });
 
-    it('/feed/featured should conform schema', () => {
-        return preq.get({
-            uri: server.config.baseURL + '/feed/featured/2016/09/08'
-        })
+    it('/feed/featured should conform schema', function() {
+        return preq.get({ uri: server.config.baseURL + '/feed/featured/2016/09/08' })
         .then(function(res) {
             if (!ajv.validate('#/definitions/feed', res.body)) {
                 throw new assert.AssertionError({
@@ -39,10 +32,8 @@ describe('Responces should conform to the provided JSON schema of the responce',
         });
     });
 
-    it('/page/summary/{title} should conform schema', () => {
-        return preq.get({
-            uri: server.config.baseURL + '/page/summary/Tank'
-        })
+    it('/page/summary/{title} should conform schema', function() {
+        return preq.get({ uri: server.config.baseURL + '/page/summary/Tank' })
         .then(function(res) {
             if (!ajv.validate('#/definitions/summary', res.body)) {
                 throw new assert.AssertionError({
@@ -51,5 +42,4 @@ describe('Responces should conform to the provided JSON schema of the responce',
             }
         });
     });
-
 });
