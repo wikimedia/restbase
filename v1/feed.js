8e6c6d39525ec6960c372ed4e483536b5379fab7
Petr Pchelko <ppchelko@wikimedia.org>
2016-10-18 11:00:10 -0700

    Store complete older content

--- v1/feed.js
+++ v1/feed.js
@@ -10,7 +10,6 @@ const HTTPError = HyperSwitch.HTTPError;
 
 const spec = HyperSwitch.utils.loadSpec(`${__dirname}/feed.yaml`);
 
-
 const DEFAULT_TTL = 3600;
 
 const FEED_URIS = {
@@ -20,10 +19,46 @@ const FEED_URIS = {
     news: { uri: ['v1', 'page', 'news'], date: false }
 };
 
+/**
+ * Checks whether the date is today or in the past in UTC-0 timezone
+ *
+ * @param {Date} date a date to check
+ * @return {boolean} true if the date is in the past
+ */
 function isHistoric(date) {
     const now = new Date();
     const today = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
     return date < today;
+}
+
+/**
+ * Safely builds the Date from request parameters
+ *
+ * @param {Object} rp request parameters object
+ * @return {Date} the requested date.
+ */
+function getDateSafe(rp) {
+    try {
+        return new Date(Date.UTC(rp.yyyy, rp.mm - 1, rp.dd));
+    } catch (err) {
+        throw new HTTPError({
+            status: 400,
+            body: {
+                type: 'bad_request',
+                description: 'wrong date format specified'
+            }
+        });
+    }
+}
+
+/**
+ * Converts the date to the bucket key, key the records in the format YYYY-MM-DD
+ *
+ * @param {Date} date the date to convert
+ * @return {string}
+ */
+function toKey(date) {
+    return date.toISOString().split('T').shift();
 }
 
 class Feed {
@@ -47,135 +82,158 @@ class Feed {
         return P.props(props);
     }
 
-    aggregated(hyper, req) {
-        const rp = req.params;
-        let date;
-        let dateArr;
+    _assembleResult(result, dateArr) {
+        // assemble the final response to be returned
+        const finalResult = {
+            status: 200,
+            headers: {
+                'cache-control': this.options.feed_cache_control,
+                // mimic MCS' ETag value
+                etag: `${dateArr.join('')}/${uuid.now().toString()}`,
+                // TODO: need a way to dynamically derive this
+                'content-type': 'application/json; charset=utf-8; ' +
+                'profile="https://www.mediawiki.org/wiki/Specs/aggregated-feed/0.5.0"'
+            },
+            body: {}
+        };
+        // populate its body
+        Object.keys(result).forEach((key) => {
+            if (result[key].body && Object.keys(result[key].body).length) {
+                finalResult.body[key] = result[key].body;
+            }
+        });
+        return finalResult;
+    }
 
-        // key the records on the date in the format YYYY-MM-DD
-        try {
-            date = new Date(Date.UTC(rp.yyyy, rp.mm - 1, rp.dd));
-            date = date.toISOString().split('T').shift();
-            dateArr = date.split('-');
-        } catch (err) {
-            throw new HTTPError({
-                status: 400,
-                body: {
-                    type: 'bad_request',
-                    description: 'wrong date format specified'
-                }
-            });
+    _populateSummaries(hyper, req, res) {
+        const feed = res.body;
+        const summaries = {};
+        const requestTitle = (title) => {
+            if (title && !summaries[title]) {
+                summaries[title] = hyper.get({
+                    uri: new URI([req.params.domain, 'v1', 'page', 'summary', title])
+                })
+                .get('body')
+                // Swallow the error, no need to fail the whole feed
+                // request because of one failed summary fetch
+                .catchReturn(undefined);
+            }
+        };
+
+        if (feed.tfa) {
+            requestTitle(feed.tfa.title);
         }
-
-        let feedRequest;
-        if (isHistoric(date)) {
-            feedRequest = hyper.get({
-                uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated.historic', date])
-            });
-        } else {
-            feedRequest = hyper.get({
-                uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated', date])
-            })
-            .catch({ status: 404 }, () => // it's a cache miss, so we need to request all
-                // of the components and store them
-                this._makeFeedRequests(Object.keys(FEED_URIS), hyper, rp, dateArr)
-                .then((result) => {
-                    // assemble the final response to be returned
-                    const finalResult = {
-                        status: 200,
-                        headers: {
-                            'cache-control': this.options.feed_cache_control,
-                            // mimic MCS' ETag value
-                            etag: `${dateArr.join('')}/${uuid.now().toString()}`,
-                            // TODO: need a way to dynamically derive this
-                            'content-type': 'application/json; charset=utf-8; ' +
-                            'profile="https://www.mediawiki.org/wiki/Specs/aggregated-feed/0.5.0"'
-                        },
-                        body: {}
-                    };
-                    // populate its body
-                    Object.keys(result).forEach((key) => {
-                        if (result[key].body && Object.keys(result[key].body).length) {
-                            finalResult.body[key] = result[key].body;
-                        }
+        if (feed.mostread && feed.mostread.articles) {
+            feed.mostread.articles.forEach((article) => { requestTitle(article.title); });
+        }
+        if (feed.news) {
+            feed.news.forEach((newsItem) => {
+                if (newsItem.links) {
+                    newsItem.links.forEach((article) => {
+                        requestTitle(article.title);
                     });
-                    // store it, and update the historic
-                    return hyper.put({
-                        uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated', date]),
-                        headers: finalResult.headers,
-                        body: finalResult.body
-                    }).then(() => finalResult);
-                }));
+                }
+            });
         }
 
-        return feedRequest
-        .then((res) => {
-            // We've got the titles, populate them with summaries
-            const feed = res.body;
-            const summaries = {};
-            const requestTitle = (title) => {
-                if (title && !summaries[title]) {
-                    summaries[title] = hyper.get({
-                        uri: new URI([rp.domain, 'v1', 'page', 'summary', title])
-                    })
-                    .get('body')
-                    // Swallow the error, no need to fail the whole feed
-                    // request because of one failed summary fetch
-                    .catchReturn(undefined);
+        return P.props(summaries)
+        .then((summaries) => {
+            const assignSummary = (article) => {
+                if (!summaries[article.title]) {
+                    return;
                 }
+                // MCS expects the title to be a DB Key
+                delete summaries[article.title].title;
+                const result = Object.assign(article, summaries[article.title]);
+                if (!result.normalizedtitle) {
+                    result.normalizedtitle = result.title.replace(/_/g, ' ');
+                }
+                return result;
             };
 
-            if (feed.tfa) {
-                requestTitle(feed.tfa.title);
+            const assignAllSummaries = (articles) => {
+                if (!articles) {
+                    return;
+                }
+                return articles.map(assignSummary).filter((article) => !!article);
+            };
+
+            feed.tfa = feed.tfa && assignSummary(feed.tfa);
+            if (feed.mostread) {
+                feed.mostread.articles = assignAllSummaries(feed.mostread.articles);
             }
-            if (feed.mostread && feed.mostread.articles) {
-                feed.mostread.articles.forEach((article) => { requestTitle(article.title); });
-            }
+
             if (feed.news) {
                 feed.news.forEach((newsItem) => {
-                    if (newsItem.links) {
-                        newsItem.links.forEach((article) => {
-                            requestTitle(article.title);
-                        });
-                    }
+                    newsItem.links = assignAllSummaries(newsItem.links);
                 });
             }
+            return res;
+        });
+    }
 
-            return P.props(summaries)
-            .then((summaries) => {
-                const assignSummary = (article) => {
-                    if (!summaries[article.title]) {
-                        return;
-                    }
-                    // MCS expects the title to be a DB Key
-                    delete summaries[article.title].title;
-                    const result = Object.assign(article, summaries[article.title]);
-                    if (!result.normalizedtitle) {
-                        result.normalizedtitle = result.title.replace(/_/g, ' ');
-                    }
-                    return result;
-                };
 
-                const assignAllSummaries = (articles) => {
-                    if (!articles) {
-                        return;
-                    }
-                    return articles.map(assignSummary).filter((article) => !!article);
-                };
+    _getHistoricContent(hyper, req, date) {
+        const rp = req.params;
+        const dateKey = toKey(date);
+        const dateArr = dateKey.split('-');
+        return hyper.get({
+            uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated.historic', dateKey])
+        })
+        .catch({ status: 404 }, () =>
+            // it's a cache miss, so we need to request all
+            // of the components and store them (but don't request news)
+            this._makeFeedRequests([ 'tfa', 'mostread', 'image' ], hyper, rp, dateArr)
+            .then((result) => this._assembleResult(result, dateArr))
+            .then((res) => this._populateSummaries(hyper, req, res))
+            .tap((res) => {
+                hyper.put({
+                    uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated.historic', date]),
+                    headers: res.headers,
+                    body: res.body
+                });
+            })
+        );
+    }
 
-                feed.tfa = feed.tfa && assignSummary(feed.tfa);
-                if (feed.mostread) {
-                    feed.mostread.articles = assignAllSummaries(feed.mostread.articles);
-                }
-
-                if (feed.news) {
-                    feed.news.forEach((newsItem) => {
-                        newsItem.links = assignAllSummaries(newsItem.links);
-                    });
-                }
-                return res;
+    _getCurrentContent(hyper, req, date) {
+        const rp = req.params;
+        const dateKey = toKey(date);
+        const dateArr = dateKey.split('-');
+        return hyper.get({
+            uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated', dateKey])
+        })
+        .catch({ status: 404 }, () =>
+            // it's a cache miss, so we need to request all
+            // of the components and store them
+            this._makeFeedRequests(Object.keys(FEED_URIS), hyper, rp, dateArr)
+            .then((result) => {
+                const finalResult = this._assembleResult(result, dateArr);
+                // Store it
+                return hyper.put({
+                    uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated', date]),
+                    headers: finalResult.headers,
+                    body: finalResult.body
+                }).thenReturn(finalResult);
+            }))
+        .then((res) => this._populateSummaries(hyper, req, res))
+        .tap((res) => {
+            hyper.put({
+                uri: new URI([rp.domain, 'sys', 'key_value', 'feed.aggregated.historic', date]),
+                headers: res.headers,
+                body: res.body
             });
         });
+    }
+
+    aggregated(hyper, req) {
+        const rp = req.params;
+        const date = getDateSafe(rp);
+        if (isHistoric(date)) {
+            return this._getHistoricContent(hyper, req, date);
+        } else {
+            return this._getCurrentContent(hyper, req, date);
+        }
     }
 }
 
