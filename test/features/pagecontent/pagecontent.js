76f5970311d058ecc81723505f67f9e1d0f0134a
Petr Pchelko <petrpchelko@gmail.com>
2016-10-20 12:46:32 -0700

    Revert #690 (#691)

--- test/features/pagecontent/pagecontent.js
+++ test/features/pagecontent/pagecontent.js
@@ -22,8 +22,7 @@ describe('item requests', function() {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(res.headers['access-control-allow-origin'], '*');
             assert.deepEqual(res.headers['access-control-allow-methods'], 'GET');
-            assert.deepEqual(res.headers['access-control-allow-headers'],
-                'accept, accept-encoding, accept-language, content-type');
+            assert.deepEqual(res.headers['access-control-allow-headers'], 'accept, content-type');
             assert.deepEqual(res.headers['access-control-expose-headers'], 'etag');
         });
     });
