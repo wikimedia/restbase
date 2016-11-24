f646340419995479d997086dd2b94917452c8bd7
Petr Pchelko <ppchelko@wikimedia.org>
2016-11-24 12:51:39 -0800

    Test for russian wiki to examine correct title encoding

--- test/features/pdf.js
+++ test/features/pdf.js
@@ -10,11 +10,13 @@ describe('Feed', () => {
 
     it('Should get PDF for a page', () => {
         return preq.get({
-            uri: `${server.config.bucketURL}/pdf/Test`
+            uri: `${server.config.hostPort}/ru.wikipedia.org/v1/page/pdf/%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80`
         })
         .then((res) => {
             assert.deepEqual(res.status, 200);
-            assert.deepEqual(res.headers['content-disposition'], 'attachment; filename=Test.pdf');
+            assert.deepEqual(res.headers['content-disposition'],
+                'attachment; filename=%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf;'
+                    + ' filename*=%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf');
             assert.deepEqual(res.headers['content-type'], 'application/pdf');
             assert.ok(/"\d+\/[\d\w-]+"/.test(res.headers.etag));
             assert.ok(res.body.length !== 0);
