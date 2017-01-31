bf918d621a7517f6b241069becedc91b70b1818b
Petr Pchelko <ppchelko@wikimedia.org>
2017-01-31 12:40:10 -0800

    Don't abstract out the actual request to the MCS

--- v1/feed.js
+++ v1/feed.js
@@ -7,6 +7,29 @@ const URI = HyperSwitch.URI;
 const mwUtil = require('../lib/mwUtil');
 const BaseFeed = require('../lib/base_feed');
 
+const PARTS_URIS = {
+    tfa: {
+        uri: ['v1', 'page', 'featured'],
+        date: true,
+        renewable: true
+    },
+    mostread: {
+        uri: ['v1', 'page', 'most-read'],
+        date: true,
+        renewable: true
+    },
+    image: {
+        uri: ['v1', 'media', 'image', 'featured'],
+        date: true,
+        renewable: true
+    },
+    news: {
+        uri: ['v1', 'page', 'news'],
+        date: false,
+        renewable: false
+    }
+};
+
 class Feed extends BaseFeed {
     // TODO: this is temporary code to increase the size of the TFA thumbnail
     _hydrateResponse(hyper, req, res) {
@@ -48,34 +71,33 @@ class Feed extends BaseFeed {
             return mwUtil.hydrateResponse(res, fetchSummary);
         }
     }
+
+    _makeFeedRequests(hyper, req, dateArr, isHistoric) {
+        const props = {};
+        const rp = req.params;
+        let parts = Object.keys(PARTS_URIS);
+        if (isHistoric) {
+            parts = parts.filter((part) => PARTS_URIS[part].renewable);
+        }
+        parts.forEach((part) => {
+            const def = PARTS_URIS[part];
+            const uriArray = [this.options.host, rp.domain].concat(def.uri);
+            if (def.date) {
+                Array.prototype.push.apply(uriArray, dateArr);
+            }
+            props[part] = hyper.get({
+                uri: uriArray.join('/'),
+                query: { aggregated: true }
+            });
+        });
+        return P.props(props);
+    }
 }
 
 const spec = HyperSwitch.utils.loadSpec(`${__dirname}/feed.yaml`);
 
 module.exports = (options) => {
     options.name = 'feed.aggregated';
-    options.part_uris = {
-        tfa: {
-            uri: ['v1', 'page', 'featured'],
-            date: true,
-            renewable: true
-        },
-        mostread: {
-            uri: ['v1', 'page', 'most-read'],
-            date: true,
-            renewable: true
-        },
-        image: {
-            uri: ['v1', 'media', 'image', 'featured'],
-            date: true,
-            renewable: true
-        },
-        news: {
-            uri: ['v1', 'page', 'news'],
-            date: false,
-            renewable: false
-        }
-    };
     // TODO: need a way to dynamically derive this
     options.content_type = 'application/json; charset=utf-8; ' +
         'profile="https://www.mediawiki.org/wiki/Specs/aggregated-feed/0.5.0"';
