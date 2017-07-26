267e1f4e04dcec4272fa4586a917d85918f53516
Gabriel Wicke <gwicke@wikimedia.org>
2017-07-26 10:46:50 -0700

    Improve filename encoding for PDF responses
    - Use quoted string syntax for the plain filename parameter. Protecting
    raw `"` remains a TODO that will require new functionality in
    hyperswitch. In the meantime, adding quotes already protects many other
    chars.
    - Correctly pass the mandatory encoding in extended filename syntax.
    This avoids Firefox picking up apostrophes in the filename as a
    delimiter.
    
    Bug: https://phabricator.wikimedia.org/T171747

--- test/features/pdf.js
+++ test/features/pdf.js
@@ -15,8 +15,8 @@ describe('PDF Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(res.headers['content-disposition'],
-                'attachment; filename=%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf;'
-                    + ' filename*=%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf');
+                'attachment; filename="%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf";'
+                    + ' filename*=UTF-8\'\'%D0%94%D0%B0%D1%80%D1%82_%D0%92%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf');
             assert.deepEqual(res.headers['content-type'], 'application/pdf');
             assert.ok(/"\d+\/[\d\w-]+"/.test(res.headers.etag));
             assert.ok(res.body.length !== 0);
