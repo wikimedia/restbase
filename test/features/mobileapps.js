13d91e06775ff040423cd451247afdb7f6e601ee
Petr Pchelko <ppchelko@wikimedia.org>
2018-02-07 08:58:06 -0800

    Fixed a typo that preventer etag to be passed for partial mobile content
    Bug: https://phabricator.wikimedia.org/T186720
    Change-Id: Ic4cafc32aef841c84004f1df07643ba461ff87b1

--- test/features/mobileapps.js
+++ test/features/mobileapps.js
@@ -18,6 +18,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(!!res.headers.etag, true);
             assert.deepEqual(!!res.body.lead, true);
             assert.deepEqual(!!res.body.remaining, true);
         });
@@ -30,6 +31,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(!!res.headers.etag, true);
         });
     });
 
@@ -40,6 +42,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(!!res.headers.etag, true);
         });
     });
 
@@ -50,6 +53,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(new RegExp(`^"${pageRev}\/.+"$`).test(res.headers.etag), true);
             assert.deepEqual(!!res.body.lead, true);
             assert.deepEqual(!!res.body.remaining, true);
             assert.deepEqual(res.body.lead.revision, pageRev);
@@ -63,6 +67,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(new RegExp(`^"${pageRev}\/.+"$`).test(res.headers.etag), true);
             assert.deepEqual(res.body.revision, pageRev);
         });
     });
@@ -74,6 +79,7 @@ describe('Mobile Content Service', () => {
         .then((res) => {
             assert.deepEqual(res.status, 200);
             assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+            assert.deepEqual(new RegExp(`^"${pageRev}\/.+"$`).test(res.headers.etag), true);
         });
     });
 });
