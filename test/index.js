16850f3f8da5497f63880e974c1f87082082337e
Petr Pchelko <ppchelko@wikimedia.org>
2018-08-06 16:15:17 -0700

    Remove jshint and jscs in favor of eslint
    Change-Id: I2d946fcb842772438f2ea9eea42b62c608cae18e

--- test/index.js
+++ test/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-
-// Run jshint as part of normal testing
-require('mocha-jshint')();
-// Run jscs as part of normal testing
-require('mocha-jscs')();
 require('mocha-eslint')([
     'lib',
     'sys',
