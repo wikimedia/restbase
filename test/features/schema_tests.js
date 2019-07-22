5ce0bad860316ba90dfef98d55593c6ddb219746
Petr Pchelko <ppchelko@wikimedia.org>
2019-07-22 10:20:33 -0700

    Store PCS endpoints.
    Bug: https://phabricator.wikimedia.org/T222384
    Change-Id: I5b7184f2b00fdf36fad55d30a6158d39ddb02dc2

--- test/features/schema_tests.js
+++ test/features/schema_tests.js
@@ -41,71 +41,75 @@ parallel('Responses should conform to the provided JSON schema of the response',
             });
     });
 
-    it('/feed/featured should conform schema', () => {
-        return preq.get({ uri: `${server.config.baseURL()}/feed/featured/${getToday()}` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/feed', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
+    [
+        {
+            path: '/feed/featured',
+            params: getToday(),
+            schema: 'feed'
+        },
+        {
+            domain: 'ru.wikipedia.org',
+            path: '/feed/featured',
+            params: getToday(),
+            schema: 'feed'
+        },
+        {
+            path: '/page/summary',
+            params: 'Tank',
+            schema: 'summary'
+        },
+        {
+            path: '/feed/announcements',
+            schema: 'announcementsResponse'
+        },
+        {
+            path: '/feed/onthisday',
+            params: 'all/01/03',
+            schema: 'onthisdayResponse'
+        },
+        {
+            path: '/page/related',
+            params: 'Tank',
+            schema: 'related'
+        },
+        {
+            path: '/page/media',
+            params: 'Tank',
+            schema: 'media_list_with_metadata'
+        },
+        {
+            path: '/page/media-list',
+            params: 'Tank',
+            schema: 'media_list'
+        },
+        {
+            path: '/page/references',
+            params: 'Tank',
+            schema: 'references_response'
+        },
+        {
+            path: '/page/metadata',
+            params: 'Tank',
+            schema: 'metadata'
+        }
+    ].forEach((testSpec) => {
+        let name = `${testSpec.path} should conform schema`;
+        if (testSpec.domain) {
+            name += `, ${testSpec.domain}`;
+        }
+        it(name, () => {
+            let path = `${server.config.baseURL(testSpec.domain)}${testSpec.path}`;
+            if (testSpec.params) {
+                path += `/${testSpec.params}`;
             }
-        });
-    });
-
-    it('/feed/featured should conform schema, ruwiki', () => {
-        return preq.get({ uri: `${server.config.baseURL('ru.wikipedia.org')}/feed/featured/${getToday()}` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/feed', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
-            }
-        });
-    });
-
-
-    it('/page/summary/{title} should conform schema', () => {
-        return preq.get({ uri: `${server.config.baseURL()}/page/summary/Tank` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/summary', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
-            }
-        });
-    });
-
-    it('/feed/announcements should conform schema', () => {
-        return preq.get({ uri: `${server.config.baseURL()}/feed/announcements` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/announcementsResponse', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
-            }
-        });
-    });
-
-    it('/feed/onthisday should conform schema', () => {
-        return preq.get({ uri: `${server.config.baseURL()}/feed/onthisday/all/01/03` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/onthisdayResponse', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
-            }
-        });
-    });
-
-
-    it('/page/related should conform schema', () => {
-        return preq.get({ uri: `${server.config.bucketURL()}/related/Tank` })
-        .then((res) => {
-            if (!ajv.validate('#/components/schemas/related', res.body)) {
-                throw new assert.AssertionError({
-                    message: ajv.errorsText()
-                });
-            }
+            return preq.get({ uri: path })
+            .then((res) => {
+                if (!ajv.validate(`#/components/schemas/${testSpec.schema}`, res.body)) {
+                    throw new assert.AssertionError({
+                        message: ajv.errorsText()
+                    });
+                }
+            });
         });
     });
 });
