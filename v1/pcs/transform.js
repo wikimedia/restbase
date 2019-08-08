1847d10047cafdc847542f2b58a439e6cbcc81fd
Petr Pchelko <ppchelko@wikimedia.org>
2019-08-08 11:43:14 -0700

    Expose /transform/wikitext/to/mobile-html endpoint.
    Bug: https://phabricator.wikimedia.org/T211026
    Change-Id: I029235b5e6ecd53d76dd1f38fcb804f24ed3772e

---
+++ v1/pcs/transform.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const HyperSwitch = require('hyperswitch');
+const URI = HyperSwitch.URI;
+
+class TransformService {
+    constructor(options) {
+        if (!options && !options.mobileapps_host) {
+            throw new Error('mobileapps_host not specified for PCS transform configuration');
+        }
+        this._options = options;
+    }
+
+    transformWikitextToMobileHtml(hyper, req) {
+        const rp = req.params;
+        return hyper.post({
+            uri: new URI(
+                [rp.domain, 'sys', 'parsoid', 'transform', 'wikitext', 'to', 'html', rp.title]
+            ),
+            headers: {
+                'accept-language': req.headers && req.headers['accept-language']
+            },
+            body: {
+                wikitext: req.body.wikitext
+            }
+        })
+        .then((res) => {
+            return hyper.post({
+                uri: new URI(`${this._options.mobileapps_host}/${rp.domain}` +
+                    `/v1/transform/html/to/mobile-html/${rp.title}`),
+                headers: {
+                    'content-type': res.headers['content-type']
+                },
+                body: res.body
+            })
+            .tap((mobileRes) => {
+                mobileRes.headers['content-language'] = res.headers['content-language'];
+            });
+        });
+    }
+}
+
+module.exports = (options) => {
+    const service = new TransformService(options);
+    const spec = HyperSwitch.utils.loadSpec(`${__dirname}/transform.yaml`);
+    return {
+        spec,
+        operations: {
+            transformWikitextToMobileHtml: service.transformWikitextToMobileHtml.bind(service)
+        }
+    };
+};
