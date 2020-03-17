88a18b8037b060c66643ab34e5af7c14612a377e
Michael Holloway <mholloway@wikimedia.org>
2020-03-16 21:51:41 -0400

    Remove PCS media, metadata, and references endpoints
    These endpoints were never used by the apps and will never be.

--- test/features/pcs.js
+++ test/features/pcs.js
@@ -4,75 +4,37 @@ const assert = require('../utils/assert.js');
 const Server = require('../utils/server.js');
 const preq   = require('preq');
 
-[
-    {
-        endpoint: 'media',
-        check: (res) => {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
-            assert.deepEqual(!!res.body.items, true);
-        }
-    },
-    {
-        endpoint: 'media-list',
-        check: (res) => {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
-            assert.deepEqual(!!res.body.items, true);
-        }
-    },
-    {
-        endpoint: 'metadata',
-        check: (res) => {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
-            assert.deepEqual(!!res.body.revision, true);
-            assert.deepEqual(!!res.body.tid, true);
-            assert.deepEqual(!!res.body.toc, true);
-            assert.deepEqual(!!res.body.language_links, true);
-            assert.deepEqual(!!res.body.categories, true);
-            assert.deepEqual(!!res.body.protection, true);
-        }
-    },
-    {
-        endpoint: 'references',
-        check: (res) => {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
-            assert.deepEqual(!!res.body.revision, true);
-            assert.deepEqual(!!res.body.tid, true);
-            assert.deepEqual(!!res.body.reference_lists, true);
-            assert.deepEqual(!!res.body.references_by_id, true);
-        }
-    }
-].forEach((testSpec) => {
-    describe(`Page Content Service: /page/${testSpec.endpoint}`, () => {
-        const server = new Server();
-        before(() => server.start());
-        after(() => server.stop());
 
-        const pageTitle = 'San_Francisco';
-        const pageRev = 395889;
+describe('Page Content Service: /page/media-list', () => {
+    const server = new Server();
+    before(() => server.start());
+    after(() => server.stop());
 
-        it(`Should fetch latest ${testSpec.endpoint}`, () => {
-            return preq.get({
-                uri: `${server.config.bucketURL()}/${testSpec.endpoint}/${pageTitle}`
-            })
+    const pageTitle = 'San_Francisco';
+    const pageRev = 395889;
+
+    it('Should fetch latest media-list', () => {
+        return preq.get({
+            uri: `${server.config.bucketURL()}/media-list/${pageTitle}`
+        })
             .then((res) => {
-                testSpec.check(res);
+                assert.deepEqual(res.status, 200);
+                assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+                assert.deepEqual(!!res.body.items, true);
                 assert.deepEqual(!!res.headers.etag, true);
             });
-        });
+    });
 
-        it(`Should fetch older ${testSpec.endpoint}`, () => {
-            return preq.get({
-                uri: `${server.config.bucketURL()}/${testSpec.endpoint}/${pageTitle}/${pageRev}`
-            })
+    it('Should fetch older media-list', () => {
+        return preq.get({
+            uri: `${server.config.bucketURL()}/media-list/${pageTitle}/${pageRev}`
+        })
             .then((res) => {
-                testSpec.check(res);
+                assert.deepEqual(res.status, 200);
+                assert.deepEqual(/^application\/json/.test(res.headers['content-type']), true);
+                assert.deepEqual(!!res.body.items, true);
                 assert.deepEqual(new RegExp(`^(?:W\/)?"${pageRev}\/.+"$`).test(res.headers.etag), true);
             });
-        });
     });
 });
 
