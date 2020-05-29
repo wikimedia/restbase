460ee9aa6fa2eafb12b9f615e1305035675ff08c
lens0021 <lorentz0021@gmail.com>
2020-05-30 07:32:06 +0900

    Support MySQL backend
    Bug: T242055

--- sys/table.js
+++ sys/table.js
@@ -9,8 +9,11 @@ module.exports = (options) => {
     options.conf.backend = options.conf.backend || 'cassandra';
     options.log = options.logger.log.bind(options.logger);
 
-    if (options.conf.backend !== 'cassandra' &&
-            options.conf.backend !== 'sqlite') {
+    if (
+        options.conf.backend !== 'cassandra' &&
+        options.conf.backend !== 'mysql' &&
+        options.conf.backend !== 'sqlite'
+    ) {
         throw new Error(`Unsupported backend version specified: ${options.conf.backend}`);
     }
 
